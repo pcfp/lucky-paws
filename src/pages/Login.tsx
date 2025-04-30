8d15d765415421ac15437698d5191cf96172bfd1
Peter Liu <70667500+pcfp@users.noreply.github.com>
2025-04-30 14:42:13 -0700

    create log in page and components

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/login-form"
+
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center ">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
