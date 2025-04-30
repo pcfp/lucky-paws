1139ac5a3e242ba94f6a4862cabaa6a1c4ebb28b
Peter Liu <70667500+pcfp@users.noreply.github.com>
2025-04-30 01:34:57 -0700

    correct base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/lucky-dogs", 
+  base: "/", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
