956e016e775073e8b95b640e426c8931e442fcdf
Peter Liu <70667500+pcfp@users.noreply.github.com>
2025-04-30 17:24:53 -0700

    add shadcn ui components

---
+++ src/components/ui/multi-select.tsx
@@ -0,0 +1,117 @@
+import * as React from "react"
+import { X } from "lucide-react"
+import { Badge } from "@/components/ui/badge"
+import { Command, CommandGroup, CommandItem } from "@/components/ui/command"
+import { Command as CommandPrimitive } from "cmdk"
+
+type MultiSelectProps = {
+  selected: string[]
+  setSelected: (selected: string[]) => void
+  options: string[]
+  placeholder?: string
+  className?: string
+}
+
+export function MultiSelect({
+  selected,
+  setSelected,
+  options,
+  placeholder = "Select options...",
+  className,
+}: MultiSelectProps) {
+  const inputRef = React.useRef<HTMLInputElement>(null)
+  const [open, setOpen] = React.useState(false)
+  const [inputValue, setInputValue] = React.useState("")
+
+  const handleUnselect = (option: string) => {
+    setSelected(selected.filter((s) => s !== option))
+  }
+
+  const handleKeyDown = React.useCallback(
+    (e: React.KeyboardEvent<HTMLDivElement>) => {
+      const input = inputRef.current
+      if (input) {
+        if (e.key === "Delete" || e.key === "Backspace") {
+          if (input.value === "" && selected.length > 0) {
+            setSelected(selected.slice(0, -1))
+          }
+        }
+        if (e.key === "Escape") {
+          input.blur()
+        }
+      }
+    },
+    [selected, setSelected]
+  )
+
+  const selectables = options.filter((option) => !selected.includes(option))
+
+  return (
+    <Command
+      onKeyDown={handleKeyDown}
+      className={`overflow-visible bg-transparent ${className}`}
+    >
+      <div className="group border border-input px-3 py-2 text-sm ring-offset-background rounded-md focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2">
+        <div className="flex gap-1 flex-wrap">
+          {selected.map((option) => (
+            <Badge
+              key={option}
+              variant="secondary"
+              className="hover:bg-secondary"
+            >
+              {option}
+              <button
+                className="ml-1 ring-offset-background rounded-full outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleUnselect(option)
+                  }
+                }}
+                onMouseDown={(e: React.MouseEvent) => {
+                  e.preventDefault()
+                  e.stopPropagation()
+                }}
+                onClick={() => handleUnselect(option)}
+              >
+                <X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
+              </button>
+            </Badge>
+          ))}
+          <CommandPrimitive.Input
+            ref={inputRef}
+            value={inputValue}
+            onValueChange={setInputValue}
+            onBlur={() => setOpen(false)}
+            onFocus={() => setOpen(true)}
+            placeholder={placeholder}
+            className="ml-2 bg-transparent outline-none placeholder:text-muted-foreground flex-1"
+          />
+        </div>
+      </div>
+      <div className="relative mt-2">
+        {open && selectables.length > 0 ? (
+          <div className="absolute w-full z-10 top-0 rounded-md border bg-popover text-popover-foreground shadow-md outline-none animate-in">
+            <CommandGroup className="h-full overflow-auto">
+              {selectables.map((option) => (
+                <CommandItem
+                  key={option}
+                  onMouseDown={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                  }}
+                  onSelect={() => {
+                    setInputValue("")
+                    setSelected([...selected, option])
+                  }}
+                  className="cursor-pointer"
+                >
+                  {option}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </div>
+        ) : null}
+      </div>
+    </Command>
+  )
+} 
